ad8fc2f892a97388714cf8537d8da47b845b2902
Rocio1604 <rocpago@gmail.com>
2025-01-17 19:48:38 +0100

    Esta es la parte de Rocío Parejo, agregue el bucle for.

---
+++ src/main/java/com/mycompany/respoitorio/Rocio.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.respoitorio;
+
+/**
+ *
+ * @author rocki
+ */
+public class Rocio {
+    public void Rocio(){
+        for(int i=0; i<100; i++){
+            System.out.println("Gonzalo apruebame Entornos");
+        }
+    }
+}
